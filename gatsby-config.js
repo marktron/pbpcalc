f0364d07b1e85069876cc1054f7ac73b09a08ce1
Mark Allen <moustache@gmail.com>
2023-03-06 11:43:23 -0500

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "src/images/favicon.svg",
       },
     },
     "gatsby-plugin-mdx",
